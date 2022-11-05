a5af5e1dc8d223e4b2a837713dd5adeb50d85c09
joeunhye <masimel33@gmail.com>
2022-11-05 22:43:20 +0900

    angle effect

--- effect4/effect4-jq.js
+++ effect4/effect4-jq.js
@@ -1,18 +1,41 @@
 //마우스 움직이기
 const cursor = $(".cursor");
+let x = 0;
+let y = 0;
+let mouseX = 0,
+	mouseY = 0,
+	angleX = 0,
+	angleY = 0,
+	fmouseX = 0,
+	fmouseY = 0;
 $(window).mousemove(function (e) {
+	x = e.pageX;
+	y = e.pageY;
+
+	// mouseX = $(window).width() / 2 - x; //마우스 X축을 가운데로 설정
+	// mouseY = $(window).width() / 2 - y; //마우스 Y축을 가운데로 설정
+
+	//마우스의 x, y축 최소값 -50, 최대값 50으로 설정
+	mouseX = Math.max(-50, Math.min(50, $(window).width() / 2 - x));
+	mouseY = Math.max(-50, Math.min(50, $(window).height() / 2 - y));
+
+	angleX = (mouseX * 12) / 100;
+	angleY = (mouseY * 12) / 100;
+
+	fmouseX += ((angleX - fmouseX) * 1) / 10;
+	fmouseY += ((angleY - fmouseY) * 1) / 10;
+
 	//출력용
 	$(".pageX").text(e.pageX);
 	$(".pageY").text(e.pageY);
+	$(".mouseX").text(mouseX);
+	$(".mouseY").text(mouseY);
+	$(".angleX").text(angleX);
+	$(".angleY").text(angleY);
+	$(".fmouseX").text(fmouseX);
+	$(".fmouseY").text(fmouseY);
 
 	gsap.to(cursor, { duration: 0.3, left: e.clientX - 5, top: e.clientY - 5 });
 
-	$(".mouseCont em").hover(
-		function () {
-			cursor.addClass("active");
-		},
-		function () {
-			cursor.removeClass("active");
-		}
-	);
+	$(".move-image").css({ transform: `translate(-50%, -50%) perspective(600px) rotateX(${fmouseY}deg) rotateY(${fmouseX}deg)` });
 });
--- effect4/effect4-js.js
+++ effect4/effect4-js.js
@@ -1,20 +1,35 @@
 const cursor = document.querySelector(".cursor");
+let x = 0,
+	y = 0,
+	mouseX = 0,
+	mouseY = 0,
+	angleX = 0,
+	angleY = 0,
+	fmouseX = 0,
+	fmouseY = 0;
 
 window.addEventListener("mousemove", function (e) {
-	document.querySelector(".pageX").innerHTML = e.pageX;
-	document.querySelector(".pageY").innerHTML = e.pageY;
+	x = e.pageX;
+	y = e.pageY;
+
+	mouseX = Math.max(-50, Math.min(50, window.innerWidth / 2 - x));
+	mouseY = Math.max(-50, Math.min(50, window.innerHeight / 2 - y));
+	angleX = (mouseX * 12) / 100;
+	angleY = (mouseY * 12) / 100;
+	fmouseX += ((angleX - fmouseX) * 1) / 10;
+	fmouseY += ((angleY - fmouseY) * 1) / 10;
+
+	document.querySelector(".pageX").textContent = x;
+	document.querySelector(".pageY").textContent = y;
+	document.querySelector(".mouseX").textContent = mouseX;
+	document.querySelector(".mouseY").textContent = mouseY;
+	document.querySelector(".angleX").textContent = angleX;
+	document.querySelector(".angleY").textContent = angleY;
+	document.querySelector(".fmouseX").textContent = Math.round(fmouseX);
+	document.querySelector(".fmouseY").textContent = Math.round(fmouseY);
 
 	gsap.to(cursor, { duration: 0.3, left: e.clientX - 5, top: e.clientY - 5 });
 
-	//오버 효과
-	let em = document.querySelectorAll(".mouseCont em");
-	em.forEach(elem => {
-		elem.addEventListener("mouseenter", () => {
-			cursor.classList.add("active");
-		});
-
-		elem.addEventListener("mouseleave", () => {
-			cursor.classList.remove("active");
-		});
-	});
+	let move = document.querySelector(".move-image");
+	move.style.transform = `translate(-50%, -50%) perspective(600px) rotateX(${fmouseY}deg) rotateY(${fmouseX}deg)`;
 });
